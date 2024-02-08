c20b8cce8adb9ef99acfcaf54c34894c612cb640
Panos15081985 <damianakis.p@gmail.com>
2024-02-08 13:20:24 +0100

    Update Config.js

--- src/Database/Config.js
+++ src/Database/Config.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZnxwNLxd1a-_l8BheHjG9UXSJGeUjbGM",
-  authDomain: "event-57f3f.firebaseapp.com",
-  projectId: "event-57f3f",
-  storageBucket: "event-57f3f.appspot.com",
-  messagingSenderId: "86506426586",
-  appId: "1:86506426586:web:18e04f5aae631aeab715e3",
-  measurementId: "G-VZ4ZDKDFNC"
+  //firebase Daten
 };
 
 const app = initializeApp(firebaseConfig);
